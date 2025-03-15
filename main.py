67e6f245cb24765dcc7c2b2c11e558db99f6ad29
malwani <98039304+malwani9@users.noreply.github.com>
2025-03-16 01:16:05 +0300

    draw player & enable movement and rotation

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import pygame
 from constants import *
-
+from player import Player
 def main():
-    pygame.get_init()
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
         screen.fill((0,0,0))
+
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
